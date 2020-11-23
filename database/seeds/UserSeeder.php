5b49553dee7519124688fbdc1a3810ccf963af14
eromero <eromero@inteduc.cl>
2020-11-23 12:56:26 -0300

    seeders

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@admin.com";
+        $user->password = Hash::make('foodzinder');
+        $user->profile = 1;
+        $user->save();
+
+    }
+}
