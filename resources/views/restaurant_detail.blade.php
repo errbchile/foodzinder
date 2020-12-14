98ff0f7fd46c980235fa216daa081e8c71885b1a
eromero <eromero@inteduc.cl>
2020-12-14 11:57:37 -0300

    se agrega el CDN de NORMALIZE

--- resources/views/restaurant_detail.blade.php
+++ resources/views/restaurant_detail.blade.php
@@ -8,6 +8,7 @@
 	<meta name="description" content="Foogra - Discover & Book the best restaurants at the best price">
 	<meta name="author" content="Ansonika">
 	<title>Mamma Pronto - Food Zinder</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
 
 	<!-- Favicons-->
 	<link rel="shortcut icon" type="image/x-icon" href="{{asset('plantilla/img/favicon.ico')}}" >
